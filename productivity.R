a00393f3d74bcf20f8dbbf610e42d8d33135b5d4
Evan Odell <evanodell91@gmail.com>
2017-12-07 12:00:20 +0000

    plots, updated productivity data

--- productivity.R
+++ productivity.R
@@ -1,15 +1,15 @@
 
-pacman::p_load("reshape2")
-pacman::p_load("readxl")
-pacman::p_load("readr")
-pacman::p_load("dplyr")
-pacman::p_load("ggplot2")
+pacman::p_load(reshape2)
+pacman::p_load(readxl)
+pacman::p_load(readr)
+pacman::p_load(dplyr)
+pacman::p_load(ggplot2)
+pacman::p_load(plm)
 
 employment_rate <- read_csv("employment_rate.csv", col_types = cols(TIME = col_character()))
 
 employment_rate$`Flag Codes` <- NULL
 employment_rate$FREQUENCY <- as.factor(employment_rate$FREQUENCY)
-
 
 employment_rate$MEASURE <- as.factor(employment_rate$MEASURE)
 
@@ -53,16 +53,58 @@ lm_prod_emp <- lm(emp_rate ~ prod_rate, data=prod_emp2)
 
 #qplot(emp_rate, prod_rate, data=prod_emp2)
 
-p1 <- ggplot(prod_emp2, aes(x=emp_rate, y=prod_rate, group = Year)) + geom_point(aes(colour = factor(Country)))
+prod_emp2$Year <- as.numeric(as.character(prod_emp2$Year))
+
+xtabs(~Year + Country, data= prod_emp2)
+
+prod_emp_sub <- prod_emp2[prod_emp2$Year >= 2005,]
+
+prod_emp_sub$Year <- as.numeric(as.character(prod_emp_sub$Year))
+
+xtabs(~Year + Country, data= prod_emp_sub)
+
+p1 <- ggplot(prod_emp_sub, aes(x=emp_rate, y=prod_rate, group = Year)) +
+  geom_point(aes(colour = factor(Country))) +
+  geom_smooth(aes(x=emp_rate, y=prod_rate, group = NULL), method = "lm") +
+  geom_point(data=prod_emp_sub[prod_emp_sub$Country=="United Kingdom",],
+             aes(x=emp_rate, y=prod_rate), shape=1, colour="black", size=2) +
+  geom_smooth(data=prod_emp_sub[prod_emp_sub$Country=="United Kingdom",],
+              aes(x=emp_rate, y=prod_rate, group = NULL), method = "lm", colour="black") +
+  ylab("Productivity in USD\n (constant prices, 2010 PPPs)") + 
+  xlab("Employment Rate (Percent)") +
+  labs(colour="Country") +
+  ggtitle("Employment Rate vs Productivity\nin the OECD, 2005 – 2016")
 
 p1
+
+ggsave(p1, filename="prod_trend.png", type = "cairo-png", width = 30, height = 18, units = "cm")
+
 
 prod_emp3 <- melt(prod_emp2)
 
 prod_emp3$Year <- as.numeric(prod_emp3$Year)
 
-prod_emp3 <- prod_emp3[prod_emp3$Year>=2000,]
+prod_emp3 <- prod_emp3[prod_emp3$Year>=2005,]
 
-prod_grid <- ggplot(prod_emp3, aes(y=value, x=Year, group = variable)) + geom_line(aes(col=variable)) + facet_wrap(~ Country, nrow = 4)
+prod_emp3$variable <- factor(prod_emp3$variable, levels=c("prod_rate", "emp_rate"), labels=c("Productivity (USD, constant prices, 2010 PPPs)", "Employment Rate (Percent)"))
+
+prod_grid <- ggplot(prod_emp3, aes(y=value, x=Year, group = variable)) +
+  geom_line(aes(col=variable)) +
+  facet_wrap(~ Country, nrow = 6) +
+  scale_x_continuous(breaks=c(2005,2010,2015)) + 
+  theme(legend.position = "bottom") + 
+  ylab("Productivity (USD)/Employment (Percent)") + 
+  xlab("Year") +
+  labs(colour="Measurements") +
+  ggtitle("Employment Rate & Productivity\nin the OECD, 2005 – 2016")
 
 prod_grid
+
+ggsave(plot=prod_grid, filename="prod_grid.png", type = "cairo-png", width = 30, height = 18, units = "cm")
+
+
+plm1 <- plm(emp_rate ~ prod_rate, data = prod_emp2, index= "Country", effect = "twoways")
+
+summary(plm1)
+
+
